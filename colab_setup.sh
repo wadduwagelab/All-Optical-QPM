2b0364c6850ae281eef857d8ac060ec49f58f021
Bantami <107250649+Bantami@users.noreply.github.com>
2022-06-15 01:13:48 +0530

    Added bacteria dataset

--- colab_setup.sh
+++ colab_setup.sh
@@ -8,11 +8,16 @@ pip -q install imgaug==0.2.6
 
 pip install -q -r /content/All-Optical-QPM/requirements.txt
 
-## Download datasets
+echo "Downloading.."
 gdown https://drive.google.com/uc?id=1ickDfs6bA-YM7RQSaMPRqFnC7YApjW8e
+echo "HeLa dataset downloaded \xE2\x9C\x94"
+gdown https://drive.google.com/uc?id=1CRaPVYVUs-vJA6SoXqeNhBRiqMwITbv6
+echo "Bacteria dataset downloaded \xE2\x9C\x94"
 
-## Download pretrained models
+# Download pretrained models
 gdown https://drive.google.com/uc?id=1tHBWjNJPRHf1VX0XJicLIKs8av43_TPz
+echo "Pretrained models downloaded \xE2\x9C\x94"
+
 mkdir /content/datasets
 unzip -qq hela.zip -d /content/datasets/
 unzip -qq models.zip -d /content/models/
