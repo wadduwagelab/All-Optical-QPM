b3ba81ad118317d08bae415adb6529c546d74d71
Bantami <107250649+Bantami@users.noreply.github.com>
2022-06-14 19:55:27 +0530

    Fix dependency issue in colab for package imgaug

--- colab_setup.sh
+++ colab_setup.sh
@@ -1,6 +1,11 @@
 # conda install pytorch==1.8.0 torchvision==0.9.0 torchaudio==0.8.0 cudatoolkit=11.1 -c pytorch -c nvidia
 # conda install -c conda-forge matplotlib
 # conda install -c conda-forge wandb
+
+## Remove incompatible version from colab and reinstall correct version
+pip -qq uninstall imgaug -y
+pip -qq install imgaug==0.2.6
+
 pip install -q -r /content/All-Optical-QPM/requirements.txt
 
 ## Download datasets
