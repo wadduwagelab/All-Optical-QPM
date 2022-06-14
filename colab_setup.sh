39ee9053c570a402de77095a19f83ada6794564a
Bantami <107250649+Bantami@users.noreply.github.com>
2022-06-15 01:19:01 +0530

    Extract bacteria

--- colab_setup.sh
+++ colab_setup.sh
@@ -8,16 +8,17 @@ pip -q install imgaug==0.2.6
 
 pip install -q -r /content/All-Optical-QPM/requirements.txt
 
-echo "Downloading.."
+# Download Datasets
 gdown https://drive.google.com/uc?id=1ickDfs6bA-YM7RQSaMPRqFnC7YApjW8e
-echo "HeLa dataset downloaded \xE2\x9C\x94"
+echo "HeLa dataset downloaded ✔"
 gdown https://drive.google.com/uc?id=1CRaPVYVUs-vJA6SoXqeNhBRiqMwITbv6
-echo "Bacteria dataset downloaded \xE2\x9C\x94"
+echo "Bacteria dataset downloaded ✔"
 
 # Download pretrained models
 gdown https://drive.google.com/uc?id=1tHBWjNJPRHf1VX0XJicLIKs8av43_TPz
-echo "Pretrained models downloaded \xE2\x9C\x94"
+echo "Pretrained models downloaded ✔"
 
 mkdir /content/datasets
 unzip -qq hela.zip -d /content/datasets/
+unzip -qq bacteria.zip -d /content/datasets/
 unzip -qq models.zip -d /content/models/
