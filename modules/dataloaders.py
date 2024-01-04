894ba26e7348b27863e152bd1a21ca703e70fdff
Kithmini Herath <50232322+KithminiHerath@users.noreply.github.com>
2024-01-04 02:11:09 -0700

    Edited bacteria data loader to output the train loader as well

--- modules/dataloaders.py
+++ modules/dataloaders.py
@@ -74,34 +74,6 @@ def get_qpm_np_dataloaders(img_size, train_batch_size ,torch_seed=10, data_dir= 
     return train_loader, val_loader, test_loader
 
 
-def get_wide_dataloaders(img_size, train_batch_size ,torch_seed=10, data_dir= '/home/ubuntu/datasets/wide_dataset', task_type= 'phase2intensity', shrinkFactor = 1, **kwargs):
-    '''
-        Function to return train, validation "Noise" dataloaders
-        Args:
-            img_size         : Image size to resize
-            train_batch_size : batch size for training
-            torch_seed       : seed
-            data_dir         : data directory which has the data hierarchy as `./train/P1024_0.npy`
-            task_type        : type of input to output conversion
-            shrinkFactor     : shrink factor for the image
-        Returns:
-            train_loader : Data loader for training
-            val_loader   : Data loader for validation
-    '''
-    
-    torch.manual_seed(torch_seed)
-    
-    my_transform= transforms.Compose([transforms.Grayscale(num_output_channels=1),transforms.ToTensor(), transforms.Resize((int(img_size//shrinkFactor), int(img_size//shrinkFactor))), transforms.CenterCrop((img_size, img_size))])
-
-    train_data = wide_dataset(data_dir=data_dir, type_= 'train', transform= my_transform, task_type= task_type)
-    val_data   = wide_dataset(data_dir=data_dir, type_= 'val'  , transform= my_transform, task_type= task_type)
-    
-    train_loader = DataLoader(train_data, batch_size = train_batch_size, shuffle=True,  drop_last= True)
-    val_loader   = DataLoader(val_data,   batch_size = 16              , shuffle=False, drop_last= True)
-
-    return train_loader, val_loader
-
-
 def get_bacteria_dataloaders(img_size, train_batch_size ,torch_seed=10, data_dir= None, task_type= 'phase2amp',shrinkFactor = 1, biasOnoise=0, photon_count=1, cfg= None, **kwargs):
     '''
         Function to return train, validation QPM dataloaders
@@ -114,19 +86,19 @@ def get_bacteria_dataloaders(img_size, train_batch_size ,torch_seed=10, data_dir
             train_loader : Data loader for training
             val_loader   : Data loader for validation
     '''
-    data_dir= '/n/holyscratch01/wadduwage_lab/fypteam_22/datasets/bacteria_np'
+    data_dir= '/n/holyscratch01/wadduwage_lab/D2NN_QPM_classification/datasets/bacteria_np'
     
     torch.manual_seed(torch_seed)
     # transforms.ToPILImage(), 
     my_transform= transforms.Compose([transforms.ToTensor(), transforms.Resize((int(img_size//shrinkFactor), int(img_size//shrinkFactor))), transforms.CenterCrop((img_size, img_size))])
 
-    #train_data = bacteria_dataset(data_dir=data_dir, type_= 'train', transform = my_transform, task_type= task_type, biasOnoise = biasOnoise, photon_count = photon_count, cfg= cfg)
+    train_data = bacteria_dataset(data_dir=data_dir, type_= 'train', transform = my_transform, task_type= task_type, biasOnoise = biasOnoise, photon_count = photon_count, cfg= cfg)
     val_data   = bacteria_dataset(data_dir=data_dir, type_= 'val',   transform = my_transform, task_type= task_type, biasOnoise = biasOnoise, photon_count = photon_count, cfg= cfg)
     test_data   = bacteria_dataset(data_dir=data_dir, type_= 'test',   transform = my_transform, task_type= task_type, biasOnoise = biasOnoise, photon_count = photon_count, cfg= cfg)
     
     
-    #train_loader = DataLoader(train_data, batch_size=train_batch_size, shuffle=True, drop_last= True)
+    train_loader = DataLoader(train_data, batch_size=train_batch_size, shuffle=True, drop_last= True)
     val_loader = DataLoader(val_data, batch_size=32, shuffle=False, drop_last= True)
     test_loader = DataLoader(test_data, batch_size=32, shuffle=False, drop_last= True)
 
-    return 0, val_loader, test_loader
+    return train_loader, val_loader, test_loader
